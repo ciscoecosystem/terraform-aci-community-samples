90b512dcbc7f7d45cdc75be593eb04c66c66ac9b
vfiftyfive <vfiftyfive@gmail.com>
2020-08-31 20:44:24 -0700

    Add aci-simple-tenant example (#1)
    * add aci-simple-tenant example
    
    * add .gitignore
    
    * add .gitignore
    
    * change code to use env variables
    
    * remove locals
    
    * run fmt

---
+++ aci-simple-tenant/main.tf
@@ -0,0 +1,35 @@
+#Use TF_ENV as a prefix for environmental variables
+variable username {}
+variable password {}
+variable url {}
+variable tenant {}
+variable bd {}
+variable vrf {}
+variable bd_subnet {}
+
+provider "aci" {
+  username = var.username
+  password = var.password
+  url      = var.url
+  insecure = true
+}
+
+resource "aci_tenant" "terraform_ten" {
+  name = var.tenant
+}
+
+resource "aci_vrf" "vrf1" {
+  tenant_dn = aci_tenant.terraform_ten.id
+  name      = var.vrf
+}
+
+resource "aci_bridge_domain" "bd1" {
+  tenant_dn          = aci_tenant.terraform_ten.id
+  relation_fv_rs_ctx = aci_vrf.vrf1.name
+  name               = var.bd
+}
+
+resource "aci_subnet" "bd1_subnet" {
+  bridge_domain_dn = aci_bridge_domain.bd1.id
+  ip               = var.bd_subnet
+}
